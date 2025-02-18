386fa40cc926572dd35fb9d6fb3d63ac5f53a936
rubyceleste <rubyceleste@live.ca>
2025-02-17 17:31:57 -0700

    cleaning code

--- scripts/climate_NPP_relationship/climate_correlations_PCA.R
+++ scripts/climate_NPP_relationship/climate_correlations_PCA.R
@@ -1,3 +1,9 @@
+library(corrplot)
+library(dplyr)
+library(factoextra)
+library(reshape2)
+library(tidyr)
+
 ## Climate correlations
 
 rm(list = ls())
@@ -6,24 +12,30 @@ rm(list = ls())
 
 # Load climate data
 load('climate/prism_clim.RData')
-prism_long = prism_long[,-8 ]
+prism_long = prism_long[,-8 ] %>% 
+  filter(year <2012)
 
 # Split by site
+#for each year for each month 
+#total unfiltered data
 clim_goose <- prism_long |>
-  dplyr::filter(loc == 'GOOSE') |>
-  dplyr::select(PPT2, Tmean2:Vpdmax2)
+  filter(loc == 'GOOSE') |>
+  select(PPT2, Tmean2:Vpdmax2)
 clim_nrp <- prism_long |>
-  dplyr::filter(loc == 'NRP') |>
-  dplyr::select(PPT2, Tmean2:Vpdmax2)
+  filter(loc == 'NRP') |>
+  select(PPT2, Tmean2:Vpdmax2)
 clim_rooster <- prism_long |>
-  dplyr::filter(loc == 'ROOSTER') |>
-  dplyr::select(PPT2, Tmean2:Vpdmax2)
+  filter(loc == 'ROOSTER') |>
+  select(PPT2, Tmean2:Vpdmax2)
 clim_sylv <- prism_long |>
-  dplyr::filter(loc == 'SYLVANIA') |>
-  dplyr::select(PPT2, Tmean2:Vpdmax2)
+  filter(loc == 'SYLVANIA') |>
+  select(PPT2, Tmean2:Vpdmax2)
 clim_harv <- prism_long |>
-  dplyr::filter(loc == 'HARVARD') |>
-  dplyr::select(PPT2, Tmean2:Vpdmax2)
+  filter(loc == 'HARVARD') |>
+  select(PPT2, Tmean2:Vpdmax2)
+clim_hmc = prism_long |>
+  filter(loc == 'HMC') |>
+  select(PPT2, Tmean2:Vpdmax2)
 
 
 
@@ -33,87 +45,97 @@ cor_nrp <- cor(clim_nrp)
 cor_rooster <- cor(clim_rooster)
 cor_sylv <- cor(clim_sylv)
 cor_harv <- cor(clim_harv)
+cor_hmc <- cor(clim_hmc)
 
 # Correlation matrices
-corrplot::corrplot(corr = cor_goose,
+corrplot(corr = cor_goose,
                    method = 'circle',
-                   type = 'upper', diag = FALSE)
-corrplot::corrplot(corr = cor_nrp,
+                   type = 'upper', diag = FALSE, 
+                   title = "goose_cor")
+corrplot(corr = cor_nrp,
                    method = 'circle',
-                   type = 'upper', diag = FALSE)
-corrplot::corrplot(corr = cor_rooster,
+                   type = 'upper', diag = FALSE,
+                   title = "NRP_cor")
+corrplot(corr = cor_rooster,
                    method = 'circle',
-                   type = 'upper', diag = FALSE)
-corrplot::corrplot(corr = cor_sylv,
+                   type = 'upper', diag = FALSE,
+                   title = "rooster_cor")
+corrplot(corr = cor_sylv,
                    method = 'circle',
-                   type = 'upper', diag = FALSE)
-corrplot::corrplot(corr = cor_harv,
+                   type = 'upper', diag = FALSE,
+                   title = "sylvania_cor")
+corrplot(corr = cor_harv,
                    method = 'circle',
-                   type = 'upper', diag = FALSE)
+                   type = 'upper', diag = FALSE,
+                   title = "harvard_cor")
+corrplot(corr = cor_hmc,
+                   method = 'circle',
+                   type = 'upper', diag = FALSE,
+                   title = "hmc_cor")
 
 ## Can use Tmean or Tmin or Tmax or Vpdmax but not any combinations of them
 
 #### Monthly as different variables ####
 
 # Split by site
-clim_goose <- prism_long |>
-  dplyr::filter(loc == 'GOOSE') |>
+clim_goose_month <- prism_long |>
+  filter(loc == 'GOOSE') |>
   # Pivot so each month and each variable has a different column
-  tidyr::pivot_wider(names_from = 'month', 
+  pivot_wider(names_from = 'month', 
                      values_from = c('PPT2', 'Tmean2', 'Tmin2', 
                                      'Tmax2', 'Vpdmax2')) |>
-  dplyr::select(-year, -loc) |>
-  tidyr::drop_na()
-clim_nrp <- prism_long |>
-  dplyr::filter(loc == 'NRP') |>
-  tidyr::pivot_wider(names_from = 'month',
+  select(-year, -loc) |>
+  drop_na()
+clim_nrp_month <- prism_long |>
+  filter(loc == 'NRP') |>
+  pivot_wider(names_from = 'month',
                      values_from = c('PPT2', 'Tmean2', 'Tmin2',
                                      'Tmax2', 'Vpdmax2')) |>
-  dplyr::select(-year, -loc) |>
-  tidyr::drop_na()
-clim_rooster <- prism_long |>
-  dplyr::filter(loc == 'ROOSTER') |>
-  tidyr::pivot_wider(names_from = 'month',
+  select(-year, -loc) |>
+  drop_na()
+clim_rooster_month <- prism_long |>
+  filter(loc == 'ROOSTER') |>
+  pivot_wider(names_from = 'month',
                      values_from = c('PPT2', 'Tmean2', 'Tmin2',
                                      'Tmax2', 'Vpdmax2')) |>
-  dplyr::select(-year, -loc) |>
-  tidyr::drop_na()
-clim_sylv <- prism_long |>
-  dplyr::filter(loc == 'SYLVANIA') |>
-  tidyr::pivot_wider(names_from = 'month',
+  select(-year, -loc) |>
+  drop_na()
+clim_sylv_month <- prism_long |>
+  filter(loc == 'SYLVANIA') |>
+  pivot_wider(names_from = 'month',
                      values_from = c('PPT2', 'Tmean2', 'Tmin2',
                                      'Tmax2', 'Vpdmax2')) |>
-  dplyr::select(-year, -loc) |>
-  tidyr::drop_na()
-clim_harv <- prism_long |>
-  dplyr::filter(loc == 'HARVARD') |>
-  tidyr::pivot_wider(names_from = 'month',
+  select(-year, -loc) |>
+  drop_na()
+clim_harv_month <- prism_long |>
+  filter(loc == 'HARVARD') |>
+  pivot_wider(names_from = 'month',
                      values_from = c('PPT2', 'Tmean2', 'Tmin2',
                                      'Tmax2', 'Vpdmax2')) |>
-  dplyr::select(-year, -loc) |>
-  tidyr::drop_na()
+  select(-year, -loc) |>
+  drop_na()
 
 # Correlations by site
-cor_goose <- cor(clim_goose)
-cor_nrp <- cor(clim_nrp)
-cor_rooster <- cor(clim_rooster)
-cor_sylv <- cor(clim_sylv)
-cor_harv <- cor(clim_harv)
+cor_goose_month <- cor(clim_goose_month)
+cor_nrp_month <- cor(clim_nrp_month)
+cor_rooster_month <- cor(clim_rooster_month)
+cor_sylv_month <- cor(clim_sylv_month)
+cor_harv_month <- cor(clim_harv_month)
 
 # Correlation matrices
-corrplot::corrplot(corr = cor_goose,
+corrplot(corr = cor_goose_month,
                    method = 'circle',
                    type = 'upper', diag = FALSE)
-corrplot::corrplot(corr = cor_nrp,
+corrplot(corr = cor_nrp_month,
                    method = 'circle',
                    type = 'upper', diag = FALSE)
-corrplot::corrplot(corr = cor_rooster,
+corrplot(corr = cor_rooster_month,
                    method = 'circle',
                    type = 'upper', diag = FALSE)
-corrplot::corrplot(corr = cor_sylv,
+corrplot(corr = cor_sylv_month,
                    method = 'circle',
                    type = 'upper', diag = FALSE)
-corrplot::corrplot(corr = cor_harv,
+corrplot(corr = cor_harv_month,
                    method = 'circle',
                    type = 'upper', diag = FALSE)
 
@@ -131,9 +153,9 @@ corrplot::corrplot(corr = cor_harv,
 
 # Pivot wider
 prism_annual <- prism_long |>
-  dplyr::group_by(year, loc) |>
+  group_by(year, loc) |>
   # Average over months
-  dplyr::summarize(PPT = mean(PPT2),
+  summarize(PPT = mean(PPT2),
                    Tmean = mean(Tmean2),
                    sd_PPT = sd(PPT2),
                    sd_Tmean = sd(Tmean2),
@@ -142,11 +164,22 @@ prism_annual <- prism_long |>
                    # Vpdmin = min(Vpdmin2),
                    Vpdmax = max(Vpdmax2)) 
 
+
 # Split by site
-clim_goose <- prism_annual |>
-  dplyr::ungroup() |>
-  dplyr::filter(loc == 'GOOSE') |>
-  dplyr::select(PPT:Vpdmax)
+clim_goose_1 <- prism_annual |>
+  ungroup() |>
+  filter(loc == 'GOOSE') |>
+  select(PPT:Vpdmax)
+
+clim_goose_year = clim_goose_1
+clim_goose_year$year = 1:nrow(clim_goose_year)
+
+goose_long = melt(clim_goose_year, id.vars = "year")
+
+ggplot()+
+  geom_line(data = goose_long, aes(x= year, y = value, colour = variable))
+#clim_goose_scaled = scale(goose_long)
+
 clim_nrp <- prism_annual |>
   dplyr::ungroup() |>
   dplyr::filter(loc == 'NRP') |>
@@ -163,6 +196,10 @@ clim_harv <- prism_annual |>
   dplyr::ungroup() |>
   dplyr::filter(loc == 'HARVARD') |>
   dplyr::select(PPT:Vpdmax)
+clim_hmc = prism_annual %>% 
+  ungroup() %>% 
+  filter(loc == "HMC") %>% 
+  select(PPT:Vpdmax)
 
 # Correlations by site
 cor_goose <- cor(clim_goose)
@@ -170,6 +207,7 @@ cor_nrp <- cor(clim_nrp)
 cor_rooster <- cor(clim_rooster)
 cor_sylv <- cor(clim_sylv)
 cor_harv <- cor(clim_harv)
+cor_hmc = cor(clim_hmc)
 
 # Correlation matrices
 corrplot::corrplot(cor = cor_goose,
@@ -187,30 +225,35 @@ corrplot::corrplot(cor = cor_sylv,
 corrplot::corrplot(cor = cor_harv,
                    method = 'circle',
                    type = 'upper', diag = FALSE)
+corrplot(cor = cor_hmc, method = 'circle', type = 'upper',
+         diag = FALSE, title = 'hmc_cor')
 
 ## # CENTROID
 library(factoextra)
 
+####GOOSE PCA################
+#scaling the dataset
 clim_goose_scaled = scale(clim_goose)
 
 clim_goose_pca = prcomp(clim_goose_scaled)
+#PCA values for PC1
 clim_goose_pca$x[,'PC1']
 
-# foo = princomp(BVC_normalized)
-
-# 
-# BVN_pca$loadings[,1:3]
-# BVN_pca$scores
-
+##????
 eigs <- clim_goose_pca$sdev^2
 rbind(SD = sqrt(eigs),
       Proportion = eigs/sum(eigs),
       Cumulative = cumsum(eigs)/sum(eigs))
 
-# png('figures/BVC_scree_plot.png')#, width = 14, height=8)
-fviz_eig(clim_goose_pca, addlabels = TRUE)
-# dev.off()
 
+#shows variables per component
+fviz_eig(clim_goose_pca, addlabels = TRUE)
+
+
+#biplot
+fviz_pca_biplot(clim_goose_pca)
+
+#visualize principal component analysis
 fviz_pca_var(clim_goose_pca, col.var = "black")
 fviz_cos2(clim_goose_pca, choice = "var", axes = 1:2)
 clim_goose_pca_time = data.frame(year=seq(1, nrow(clim_goose)), clim_goose_scaled %*% clim_goose_pca$rotation)
@@ -218,15 +261,136 @@ clim_goose_pca_time = data.frame(year=seq(1, nrow(clim_goose)), clim_goose_scale
 ggplot(data=clim_goose_pca_time) +
   geom_line(aes(x=year, y=PC1))
 
+ggplot(data = goose_long, aes(x= year, y = value))+
+  geom_line()+
+  facet_wrap(~variable, scales = "free_y")
+
 pca_time_melt = melt(clim_goose_pca_time, id.vars='year')
 pca_time_melt$type = rep('goose')
 
+####ROOSTER PCA#############
+
+clim_rooster_pca = prcomp(clim_rooster,scale. = TRUE)
+summary(clim_rooster_pca)
+clim_rooster_pca$x[,'PC1']
+
+##????
+eigs <- clim_rooster_pca$sdev^2
+rbind(SD = sqrt(eigs),
+      Proportion = eigs/sum(eigs),
+      Cumulative = cumsum(eigs)/sum(eigs))
 
 
+#shows variables per component
+fviz_eig(clim_rooster_pca, addlabels = TRUE)
+
+#biplot
+fviz_pca_biplot(clim_rooster_pca)
+
+#visualize principal component analysis
+fviz_pca_var(clim_rooster_pca, col.var = "black")
+fviz_cos2(clim_rooster_pca, choice = "var", axes = 1:2)
+#clim_goose_pca_time = data.frame(year=seq(1, nrow(clim_rooster)), clim_goose_scaled %*% clim_goose_pca$rotation)
+
+#ggplot(data=clim_rooster_pca_time) +
+ # geom_line(aes(x=year, y=PC1))
+
+# pca_time_melt = melt(clim_goose_pca_time, id.vars='year')
+# pca_time_melt$type = rep('goose')
+
+####HARVARD PCA#####
+clim_harvard_pca = prcomp(clim_harv,scale. = TRUE)
+summary(clim_harvard_pca)
+clim_harvard_pca$x[,'PC1']
+
+##????
+eigs <- clim_harvard_pca$sdev^2
+rbind(SD = sqrt(eigs),
+      Proportion = eigs/sum(eigs),
+      Cumulative = cumsum(eigs)/sum(eigs))
+
+
+#shows variables per component
+fviz_eig(clim_harvard_pca, addlabels = TRUE)
+
+#biplot
+fviz_pca_biplot(clim_harvard_pca)
+
+#visualize principal component analysis
+fviz_pca_var(clim_harvard_pca, col.var = "black")
+fviz_cos2(clim_harvard_pca, choice = "var", axes = 1:2)
+#clim_harvard_pca_time = data.frame(year=seq(1, nrow(clim_rooster)), clim_goose_scaled %*% clim_goose_pca$rotation)
+
+####NRP PCA####
+clim_nrp_pca = prcomp(clim_nrp,scale. = TRUE)
+summary(clim_nrp_pca)
+clim_nrp_pca$x[,'PC1']
+
+##????
+eigs <- clim_nrp_pca$sdev^2
+rbind(SD = sqrt(eigs),
+      Proportion = eigs/sum(eigs),
+      Cumulative = cumsum(eigs)/sum(eigs))
+
+
+#shows variables per component
+fviz_eig(clim_nrp_pca, addlabels = TRUE)
+
+#biplot
+fviz_pca_biplot(clim_nrp_pca)
+
+#visualize principal component analysis
+fviz_pca_var(clim_nrp_pca, col.var = "black")
+fviz_cos2(clim_nrp_pca, choice = "var", axes = 1:2)
+
+
+####SYLVANIA PCA#########
+clim_sylv_pca = prcomp(clim_sylv,scale. = TRUE)
+summary(clim_sylv_pca)
+clim_sylv_pca$x[,'PC1']
+
+##????
+eigs <- clim_sylv_pca$sdev^2
+rbind(SD = sqrt(eigs),
+      Proportion = eigs/sum(eigs),
+      Cumulative = cumsum(eigs)/sum(eigs))
+
+
+#shows variables per component
+fviz_eig(clim_sylv_pca, addlabels = TRUE)
+
+#biplot
+fviz_pca_biplot(clim_sylv_pca)
+
+#visualize principal component analysis
+fviz_pca_var(clim_sylv_pca, col.var = "black")
+fviz_cos2(clim_sylv_pca, choice = "var", axes = 1:2)
+#
+
+
+####HMC PCA####
+clim_hmc_pca = prcomp(clim_hmc,scale. = TRUE)
+summary(clim_hmc_pca)
+clim_hmc_pca$x[,'PC1']
+
+##????
+eigs <- clim_hmc_pca$sdev^2
+rbind(SD = sqrt(eigs),
+      Proportion = eigs/sum(eigs),
+      Cumulative = cumsum(eigs)/sum(eigs))
+
+
+#shows variables per component
+fviz_eig(clim_hmc_pca, addlabels = TRUE)
+
+#biplot
+fviz_pca_biplot(clim_hmc_pca)
+
+#visualize principal component analysis
+fviz_pca_var(clim_hmc_pca, col.var = "black")
+fviz_cos2(clim_hmc_pca, choice = "var", axes = 1:2)
 ## Can use mean_PPT, mean_Tmean, sd_Tmean, mean_Vpdmax I think
-
 #### Growing year ####
-
 ## Instead of calendar year
 
 # Pivot wider
@@ -242,7 +406,6 @@ prism_growing <- prism_long |>
                    sd_Tmean = sd(Tmean2),
                    Tmin = min(Tmin2),
                    Tmax = max(Tmax2),
-                   Vpdmin = min(Vpdmin2),
                    Vpdmax = max(Vpdmax2))
 
 # Split by site
