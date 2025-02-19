f07bccd24399dbf78aa5ad7e8bf5706a31678898
rubyceleste <rubyceleste@live.ca>
2025-02-19 12:10:47 -0700

    winter PCA

--- PCA_model.R
+++ PCA_model.R
@@ -10,8 +10,12 @@ AGBI_PCA = AGBI_data %>%
 
 models = AGBI_PCA %>% 
   group_by(site, taxon) %>% 
-  do(mod = lm(AGBI.mean ~ PCA1_summer + PCA2_summer, data=.)) 
+  do(mod = lm(AGBI.mean ~ PCA1_summer + PCA2_summer +
+                PCA1_winter + PCA2_winter, data=.)) 
  
+
+ggplot()+
+  geom_point(data = coefficients_summary, aes(x = Rsq, y = taxon, color =site))
   
 #print(models$mod)
 
@@ -20,6 +24,8 @@ coefficients_summary = models %>%
     intercept = coef(mod)[1],
     slope_PCA1s = coef(mod)[2],
     slope_PCA2s = coef(mod)[3],
+    slope_PCA1w = coef(mod)[4],
+    slope_PCA2w = coef(mod)[5],
     Rsq = summary(mod)$r.squared
   ) %>% 
   mutate(taxon = models$taxon, site = models$site)
