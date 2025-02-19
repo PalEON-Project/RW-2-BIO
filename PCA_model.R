f588df883c3decc2d10a38c6a3653559f86aaa1e
rubyceleste <rubyceleste@live.ca>
2025-02-19 11:07:40 -0700

    PCA for all sites

--- PCA_model.R
+++ PCA_model.R
@@ -5,13 +5,11 @@ pca_sites = readRDS("pca_sites.RDS")
 
 
 AGBI_PCA = AGBI_data %>% 
-  inner_join(goose_PCA, by= (c('year', 'site')))
-
-#mod = lm(AGBI.mean ~ PCA1_summer + PCA2_summer, data=AGBI_PCA)
+  inner_join(pca_sites, by= (c('year', 'site')))
 
 
 models = AGBI_PCA %>% 
-  group_by(taxon) %>% 
+  group_by(site, taxon) %>% 
   do(mod = lm(AGBI.mean ~ PCA1_summer + PCA2_summer, data=.)) 
  
   
@@ -24,7 +22,7 @@ coefficients_summary = models %>%
     slope_PCA2s = coef(mod)[3],
     Rsq = summary(mod)$r.squared
   ) %>% 
-  mutate(taxon = models$taxon)
+  mutate(taxon = models$taxon, site = models$site)
 
 # Print the summary
 print(coefficients_summary)
